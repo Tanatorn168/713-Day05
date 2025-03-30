65afc6a0dac4b080b5a2dbdbe928d4c35813a396
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-30 10:26:55 +0700

    task6

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -10,7 +10,12 @@ router.get("/", async (req, res) => {
     const pageNo = parseInt(req.query.pageNo as string) || 1;
     const keyword = req.query.keyword as string;
     const result = await service.getAllEventsWithPagination(keyword,pageSize, pageNo);
-    
+    if (result.events.length === 0) {
+                res.status(404).send("No event found");
+                return;
+            }
+             res.setHeader("x-total-count", result.count.toString());
+        
     res.setHeader("x-total-count", result.count.toString());
     res.json(result.events);
 
